0d978f08c08056b77e0f0af6c273e023e2d56961
Alex Kale <hi@awkale.me>
2015-12-08 10:17:33 -0500

    removed accepts_nested_attributes

--- app/models/media_point.rb
+++ app/models/media_point.rb
@@ -4,7 +4,7 @@ class MediaPoint < ActiveRecord::Base
   has_many :products,
             through: :media_point_products
 
-  accepts_nested_attributes_for :media_point_products
+  # accepts_nested_attributes_for :media_point_products
                                 # :reject_if => :all_blank,
                                 # :allow_destroy => true
   # accepts_nested_attributes_for :products
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   has_many :media_points,
             through: :media_point_products
 
-  accepts_nested_attributes_for :media_point_products
+  # accepts_nested_attributes_for :media_point_products
             # :reject_if => :all_blank,
             # :allow_destroy => true
   # accepts_nested_attributes_for :media_points
