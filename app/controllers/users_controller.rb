3cd506354d2329552f721a372991568042dac4a5
Alex Kale <hi@awkale.me>
2015-12-16 19:21:51 -0500

    add edit user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    user_params = params.require(:user).permit(:first_name, :last_name)
+    @user.update_attributes(user_params)
+
+    redirect_to user_path(id: @user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
