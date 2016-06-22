2e67e672a9e8dfb4c6b020e24d543f0da2dab2da
Alex Kale <alex.kale@prnewswire.com>
2016-06-22 11:23:53 -0400

    make join table unique

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -2,5 +2,7 @@ class Distribution < ActiveRecord::Base
   belongs_to :product
   belongs_to :recipient
 
+  validates_uniqueness_of :recipient_id, :scope => :product_id
+
   # default_scope { order('product_id') }
 end
