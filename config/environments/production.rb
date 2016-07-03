e984ff9a586fd0e7c06417361eb47b8500abfc0f
Alex Kale <alex.kale@prnewswire.com>
2016-07-03 19:14:01 -0400

    add config var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'prn-product.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['MAILER_DEFAULT_URL'] }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
