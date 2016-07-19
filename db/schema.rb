24e75e96a2769766266d863d8c426df84a8432c7
Alex Kale <alex.kale@prnewswire.com>
2016-07-19 17:03:46 -0400

    PRND-21 #comment - lock account after 5 tries with warning

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711220913) do
+ActiveRecord::Schema.define(version: 20160719175316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,6 +177,9 @@ ActiveRecord::Schema.define(version: 20160711220913) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.integer  "failed_attempts",        default: 0,     null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
