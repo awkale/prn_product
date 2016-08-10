1b50c31d93d3ee87a00c14ebe46f53500a4f4742
Alex Kale <alex.kale@prnewswire.com>
2016-08-10 15:46:11 -0400

    remove turbolinks and spinner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,21 +12,16 @@
 //
 //= require jquery
 //= require jquery-ui
-//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require cocoon
 //= require chosen-jquery
 //= require scaffold
-//= require turbolinks
 //= require jquery.ui.nestedSortable
 //= require sortable_tree/initializer
 //= require jquery.mmenu.min
 //= require jquery.mmenu.navbars
-//= require jquery.mmenu.turbolinks
 //= require_tree .
-
 
 $(function(){
   var $table = $('table'),
@@ -34,49 +29,42 @@ $(function(){
       $spinner = $('.spinner'),
       $limit = $('#limit'),
       $limit_form = $('#limit_form');
+
   // pagination - per page select and spinner
   $limit.change(function() {
     $limit_form.submit();
     setTimeout(function() {
       loadAnimation('loading');
-      }, 1000
+      }, 0
     );
   });
 
 
-  // hide spinner
-  $spinner.hide();
+  // var loadAnimation = function(speed) {
+  //   if (speed == 'loading') {
+  //     $container.addClass('faded');
+  //     $spinner.show();
+  //   } else {
+  //     $container.removeClass('faded');
+  //     $spinner.hide();
+  //   }
+  // }
 
-  var loadAnimation = function(speed) {
-    if (speed == 'loading') {
-      $container.addClass('faded');
-      $spinner.show();
-    } else {
-      $container.removeClass('faded');
-      $spinner.hide();
-    }
-  }
+  // setTimeout(function() {
+  //   loadAnimation('loading');
+  //   }, 0
+  // );
 
-
-  $(document).on("page:fetch", function(){
-    setTimeout(function() {
-      loadAnimation('loading');
-      }, 1000
-    );
-  });
-
-  $(document).on("page:receive", function(){
-    setTimeout(function() {
-      loadAnimation('');
-      }, 1000
-    );
-  });
+  // $(window).load(function(){
+  //   setTimeout(function() {
+  //     loadAnimation('');
+  //     }, 0
+  //   );
+  // });
 
   $('.collapse').on('show.bs.collapse', function(e) {
-    console.log('open');
     if ($(this).is(e.target)) {
       var icon = $('a[href="#'+ $(this).attr('id') + '"] i');
-      console.log(icon);
       if (icon.hasClass('bt-angle-right')) {
         icon.removeClass('bt-angle-right').addClass('bt-angle-down');
       }
@@ -84,7 +72,6 @@ $(function(){
   });
 
   $('.collapse').on('hide.bs.collapse', function(e) {
-    console.log('close');
     if ($(this).is(e.target)) {
       var icon = $('a[href="#'+ $(this).attr('id') + '"] i');
       if (icon.hasClass('bt-angle-down')) {
