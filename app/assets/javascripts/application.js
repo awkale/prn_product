956993014565cae814d5d290d82c6d29880181bc
Alex Kale <alex.kale@prnewswire.com>
2016-08-10 15:46:11 -0400

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,21 +12,16 @@
 //
 //= require jquery
 //= require jquery-ui
-//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require cocoon
 //= require chosen-jquery
 //= require scaffold
-//= require turbolinks
 //= require jquery.ui.nestedSortable
 //= require sortable_tree/initializer
 //= require jquery.mmenu.min
 //= require jquery.mmenu.navbars
-//= require jquery.mmenu.turbolinks
 //= require_tree .
-
 
 $(function(){
   var $table = $('table'),
@@ -34,41 +29,33 @@ $(function(){
       $spinner = $('.spinner'),
       $limit = $('#limit'),
       $limit_form = $('#limit_form');
+
   // pagination - per page select and spinner
   $limit.change(function() {
     $limit_form.submit();
     setTimeout(function() {
       loadAnimation('loading');
-      }, 1000
+      }, 0
     );
   });
 
 
-  // hide spinner
-  $spinner.hide();
-
-  var loadAnimation = function(speed) {
-    if (speed == 'loading') {
-      $container.addClass('faded');
-      $spinner.show();
-    } else {
-      $container.removeClass('faded');
-      $spinner.hide();
+  $('.collapse').on('show.bs.collapse', function(e) {
+    if ($(this).is(e.target)) {
+      var icon = $('a[href="#'+ $(this).attr('id') + '"] i');
+      if (icon.hasClass('bt-angle-right')) {
+        icon.removeClass('bt-angle-right').addClass('bt-angle-down');
+      }
     }
-  }
-
-
-  $(document).on("page:fetch", function(){
-    setTimeout(function() {
-      loadAnimation('loading');
-      }, 1000
-    );
   });
 
-  $(document).on("page:receive", function(){
-    setTimeout(function() {
-      loadAnimation('');
-      }, 1000
-    );
+  $('.collapse').on('hide.bs.collapse', function(e) {
+    if ($(this).is(e.target)) {
+      var icon = $('a[href="#'+ $(this).attr('id') + '"] i');
+      if (icon.hasClass('bt-angle-down')) {
+        icon.removeClass('bt-angle-down').addClass('bt-angle-right');
+      }
+    }
   });
+
 });
