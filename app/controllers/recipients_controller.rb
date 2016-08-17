f111737e176d2e7c7787bf1c9930cb3d5b3d0fdc
Alex Kale <alex.kale@prnewswire.com>
2016-08-17 16:17:59 -0400

    PRND-25 #comment sort that 💩

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,6 +1,7 @@
 class RecipientsController < ApplicationController
   before_action :find_recipient, :only => [:show, :edit, :update, :destroy]
   layout 'page'
+  helper_method :sort_column, :sort_direction
 
   def index
     @recipients = Recipient.all
@@ -8,15 +9,15 @@ class RecipientsController < ApplicationController
     @categories = Category.all
     @subjects = Subject.all
     @multimedia = Multimedium.all
-
+    params[:sort] ||= 'sort_by_name'
 
     if params[:search]
       @recipients = @recipients.search(params[:search])
-      @recipients = @recipients.order(:sort_by_name).includes(:category, :multimedia, :renderings).page(params[:page])
+      @recipients = @recipients.order(sort_column + ' ' + sort_direction).includes(:category, :multimedia, :renderings).page(params[:page])
     elsif params[:limit]
-      @recipients = Recipient.order(:sort_by_name).includes(:category, :multimedia, :renderings).page(params[:page]).per(params[:limit])
+      @recipients = Recipient.order(sort_column + ' ' + sort_direction).includes(:category, :multimedia, :renderings).page(params[:page]).per(params[:limit])
     else
-      @recipients = Recipient.order(:sort_by_name).includes(:category, :multimedia, :renderings).page(params[:page])
+      @recipients = Recipient.order(sort_column + ' ' + sort_direction).includes(:category, :multimedia, :renderings).page(params[:page])
     end
 
     respond_to do |format|
@@ -73,6 +74,14 @@ class RecipientsController < ApplicationController
   private
   def find_recipient
     @recipient = Recipient.find(params[:id])
+  end
+
+  def sort_column
+    Recipient.column_names.include?(params[:sort]) ? params[:sort] : 'sort_by_name'
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
   def recipient_params
