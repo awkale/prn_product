9212ed738a7664119c2c2448d5d1cb9cd9c98d95
Alex Kale <alex.kale@prnewswire.com>
2016-09-02 16:54:22 -0400

    complete show views sorting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :find_category, :only => [:show, :edit, :update, :destroy]
   layout 'page'
-  helper_method :sort_column, :sort_direction
 
   def index
     @categories = Category.all
@@ -12,19 +11,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    params[:sort] ||= 'sort_by_name'
-
-    @recipients = @category.recipients
-    if params[:limit]
-      @related_recipients = @recipients.order(sort_column + ' ' + sort_direction).includes(:multimedia, :renderings).page(params[:page]).per(params[:limit])
-    else
-      @related_recipients = @recipients.order(sort_column + ' ' + sort_direction).includes(:multimedia, :renderings).page(params[:page])
-    end
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    @related_recipients = @category.recipients
+    @search = @related_recipients.ransack(params[:q])
+    @search.sorts = 'sort_by_name asc' if @search.sorts.empty?
+    @related_recipients = @search.result(distinct: true)
+                         .includes(:category, :multimedia)
+                         .page(params[:page]).per(params[:limit])
   end
 
   def create
@@ -69,14 +61,6 @@ class CategoriesController < ApplicationController
   private
   def find_category
     @category = Category.find(params[:id])
-  end
-
-  def sort_column
-    Recipient.column_names.include?(params[:sort]) ? params[:sort] : 'sort_by_name'
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
   def category_params
