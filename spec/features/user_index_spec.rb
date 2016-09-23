9b9c3fc6e4c4bcb1e1d59836aa43f7bc912e8a3e
Alex Kale <alex.kale@prnewswire.com>
2016-09-23 09:58:46 -0400

    fix default test user role

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'User index page', :devise do
   scenario 'user sees own email address' do
-    user = FactoryGirl.create(:user, :admin)
-    login_as(user, scope: :user)
+    user = FactoryGirl.create(:user, :user_internal)
+    login_as(user, scope: :user_internal)
     visit users_path
     expect(page).to have_content user.email
   end
