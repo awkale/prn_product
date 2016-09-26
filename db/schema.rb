8771bfd1f9160f250b1c0f964bfbc8bce409b8b3
Alex Kale <alex.kale@prnewswire.com>
2016-09-26 15:21:11 -0400

    drop admin table

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923144128) do
+ActiveRecord::Schema.define(version: 20160926191451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",              default: "",    null: false
-    t.string   "encrypted_password", default: "",    null: false
-    t.integer  "sign_in_count",      default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.boolean  "god_mode",           default: false, null: false
-    t.boolean  "read_only",          default: false, null: false
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
