4e39ce1452f736a78aeca08ac2557d9bf6fe6d61
Alex Kale <alex.kale@prnewswire.com>
2016-10-24 17:48:30 -0400

    new sub nav

---
+++ spec/features/taxonomy_nav_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature "visiting the recipients section" do
+  scenario "the visitor sees sub nav" do
+    visit recipients_path
+    expect(page).to have_text("categories industries subjects")
+  end
+end
