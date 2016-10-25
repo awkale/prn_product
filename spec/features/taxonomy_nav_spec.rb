70f56e16ccbd6ebcb6ddc6b892cfee6b403c253e
Alex Kale <alex.kale@prnewswire.com>
2016-10-25 18:59:18 -0400

    fix spec

--- spec/features/taxonomy_nav_spec.rb
+++ spec/features/taxonomy_nav_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "visiting the recipients section" do
   scenario "the visitor sees sub nav" do
     visit recipients_path
-    expect(page).to have_text("categories industries subjects")
+    expect(page).to have_text("Industries Subjects Categories")
   end
 end
