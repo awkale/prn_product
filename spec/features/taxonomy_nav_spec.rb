05d0876a3fab34f72df9cf59c609319e49d70769
Alex Kale <alex.kale@prnewswire.com>
2016-10-26 10:10:20 -0400

    add login to spec

--- spec/features/taxonomy_nav_spec.rb
+++ spec/features/taxonomy_nav_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 feature "visiting the recipients section" do
   scenario "the visitor sees sub nav" do
+    user = FactoryGirl.create(:user, :admin_adv)
+    login_as(user, scope: :user)
     visit recipients_path
     expect(page).to have_text("Industries Subjects Categories")
   end
